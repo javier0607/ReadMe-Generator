ffa543f4e981797082c0896919f4797f3d08ba92
unknown <javi.mariscal13@gmail.com>
2022-03-15 21:43:35 -0400

    Finished questions

--- Develop/index.js
+++ Develop/index.js
@@ -7,6 +7,32 @@ const questions = [
         name: 'title',
         message: 'What is the title of your app?',
     },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of your app?',
+    },
+    {
+        type: 'input',
+        message: 'What is the installation process?',
+        name: 'install'
+    },
+    {
+        type: 'list',
+        message: 'What is the license for your app?',
+        name: 'license',
+        choices: ["Apache","ISC","MIT","Open","GNU","Mozilla","N/A"],
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+    },
+    {
+        type: 'input',
+        name: 'gitUserName',
+        message: 'What is your GitHub user name?',
+    },
 ];
 
 // TODO: Create a function to write README file
